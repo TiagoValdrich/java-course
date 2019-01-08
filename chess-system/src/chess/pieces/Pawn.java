e5427b7d12f9fb16f54e4e43c074d95c176ff9b2
Tiago Valdrich <tiago.valdrich@hotmail.com>
2019-01-08 15:48:10 -0200

    Creating the class Queen

--- chess-system/src/chess/pieces/Pawn.java
+++ chess-system/src/chess/pieces/Pawn.java
@@ -41,7 +41,7 @@ public class Pawn extends ChessPiece {
                 mat[p.getRow()][p.getColumn()] = true;
             }
             p.setValues(this.position.getRow() + 2, this.position.getColumn());
-            Position p2 = new Position(this.position.getRow() - 1, this.position.getColumn());
+            Position p2 = new Position(this.position.getRow() + 1, this.position.getColumn());
             if (this.getBoard().positionExists(p) && !this.getBoard().thereIsAPiece(p) && this.getBoard().positionExists(p) && !this.getBoard().thereIsAPiece(p) && this.getMoveCount() == 0) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
