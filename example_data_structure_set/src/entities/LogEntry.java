3ec1dce603bacbc4e650e0b3f1af260669ffda29
Tiago Valdrich <tiago.valdrich@hotmail.com>
2019-01-16 22:38:20 -0200

    Example about 'Set' data structure.

---
+++ example_data_structure_set/src/entities/LogEntry.java
@@ -0,0 +1,55 @@
+package entities;
+
+import java.util.Date;
+
+public class LogEntry {
+
+    private String username;
+    private Date data;
+
+    public LogEntry(String username, Date data) {
+        this.username = username;
+        this.data = data;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        LogEntry other = (LogEntry) obj;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        return true;
+    }
+}
